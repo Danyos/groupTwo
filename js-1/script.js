70280028ec52c3e6709496b62eb25aa522babeaf
Danyos <daniel.hambardzumyan@mail.ru>
2023-03-14 11:58:05 +0400

    first lesson

---
+++ js-1/script.js
@@ -0,0 +1,110 @@
+// let a=100
+// a=a.toString()
+
+// a=a * a.length
+
+// console.log(a)
+// vs
+// let a=100
+// a=a*a.toString().length
+// console.log(a)
+
+// console.log(11%6)
+// 2**4
+// 2*2*2*2
+
+// let x=7
+//
+// console.log(x++)
+// console.log(x)
+//
+// let y=18
+//
+// console.log(++y)
+
+
+let x="7$"
+let y="8$"
+let z=60
+x=parseFloat(x)
+y=parseFloat(y)
+z=parseFloat(z)
+
+
+
+
+x=x+y   //15
+x=z/x   //4
+y=y/x //2
+z=(z/y )/(x+y) //5
+x+=z+x+y //15
+let k=0
+
+if(x%2===0){
+    k=(x*(z+y))/100
+}else{
+    k=(x*(x+z))/100
+}
+
+
+
+x+=parseInt(k)
+
+
+
+//
+
+
+//
+// console.log(x)
+// console.log(y)
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+// // number
+// let num=255
+// // parseInt()
+// // parseFloat()
+//
+
+// toFixed()
+
+// string
+let name="Arkadi"
+
+
+
+
+
+// boolion
+// undefined
+// null
+
+// object
+
+
+
+
+
+
+
+
+
+
+// https://learn.javascript.ru/operators
+// https://learn.javascript.ru/comparison
+// https://learn.javascript.ru/ifelse
+
+// https://www.w3schools.com/js/js_strings.asp
+//https://www.w3schools.com/js/js_string_methods.asp
+//https://www.w3schools.com/js/js_string_search.asp
+//https://www.w3schools.com/js/js_string_templates.asp
