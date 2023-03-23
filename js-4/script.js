d3063350d661cd8eeddc97303cfba0f273c85cc3
Danyos <daniel.hambardzumyan@mail.ru>
2023-03-23 12:04:13 +0400

    array

---
+++ js-4/script.js
@@ -0,0 +1,120 @@
+// let arr=[1,4,5,7,8,9,3,2,4,7]
+
+// changer
+// arr.push(17)
+// arr.pop()
+// arr.shift()
+// arr.unshift(11)
+// arr.reverse()
+// arr.splice(4,1)
+
+// unchanger
+// arr=arr.slice(4,8)
+// arr.indexOf(8)
+//     ...
+
+
+
+
+
+// arr.forEach() // uxaki clickl e
+// arr.map()    // qo zangvacy poxox e yst qanakai
+// let x=arr.map(res=>res*2)
+// console.log(x)
+// arr.filter()
+// let f=arr.filter(res=>res%2)
+// console.log(f)
+
+// let r=arr.reduce((previousValue, currentValue)=>{
+//     previousValue+=currentValue
+//
+//     return previousValue
+// },0)
+// console.log(r)
+
+
+
+
+
+
+
+
+
+
+
+
+// ___________Primitive Type_______________
+// let str="Barev vonces"
+// let num=48
+// let deb=true
+// str.toLowerCase()
+// console.log(str)
+// num=num.toFixed(3)
+// console.log(num)
+
+// let arr=[1,4,5,7,8,9,3,2,4,7,8,9,18,17,14,15,16,19,10,11]
+// arr=arr.join("-")
+// console.log(arr)
+
+
+// let str='1-4-5-7-8-9-3-2-4-7-8-9-18-17-14-15-16-19-10-11'
+// let x=str.split("-")
+// console.log(x)
+
+// let card='4318280100010492'
+
+
+// let newCard=''
+// for (let i=0;i<card.length;i++){
+//     if (i%4===0 && i>1){
+//         newCard+='-'
+//     }
+//         newCard+=card[i]
+// }
+// console.log(newCard)
+
+// let arr=[1,4,5,7,8,9,3,2,4,7,8,9,18,17,14,15,16,19,10,11]
+// arr[1]='84'
+// console.log(arr)
+
+let card='Ando'
+card=card.split('').reverse().join('')
+console.log(card)
+
+
+
+
+
+
+
+// (for) kam (for of)
+// 1. drakan tareri mijin tvabanakan@ //[1,5,-1,7,-5,-4,9,8,7,2,-10], tveri gumary bajanac qanaki
+// 2.1. zuyg index unecox tareri gumary
+// 2.2. zuyg index unecox tareri artadryal@
+// 2.3. kent tar unecox indexneri gumary
+// 3. drakan ev bacasakan tarri qanak@ //[1,5,-1,7,-5,-4,9,8,7,2,-10]  //7//4
+// 4. gtnel ayn tary vor ir indexi vra kbajanvi anmnacord
+// [1,5,4,15,-5,-4,9,8,7,2,-10]// [4,15]
+// 5.gtnel zagvaci metsaguyn tary
+// 6. gtnel zagvaci poqraguyn tary
+// 7. zangvaci 2 rd amenaets arjeqy
+// 8. gtnel zagvaci mej ayn tarery
+// voronq chunen ayd zangvaci mej voreve andam vor kbajanvi ir vra anmnacord
+
+// tnayin
+// [1,"ergfe3tr",'true',true,false,-78,58,4,54,5,"5652","barev"]
+// 1. zasngvaci unenq zangvac //veradarcnel maqur tverov zangvac
+// 2. hashvel tveri gumary
+// 3. tvery verapoxer **2
+
+// ********bonus***********
+// [45,7,8,9,4,2,8,56,5,4,2,57,55,7,7]
+// 1.stanal xary tverov zangvac minchev 100 mijakayqum
+// 2, maqrel zangvacy krknvoxneric
+// 3. aradznacnel tvyal zanvaci zuygery ev kentery even ev odd zangvacnerum
+
+
+
+
+
+
